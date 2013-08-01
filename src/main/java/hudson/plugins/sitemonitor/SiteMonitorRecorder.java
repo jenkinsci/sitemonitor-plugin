14994af872d9e376f2d841f03de414a9e771487d
prithishchanda <prithish.chanda@gmail.com>
2013-08-01 16:22:59 -0500

    Update SiteMonitorRecorder.java

--- src/main/java/hudson/plugins/sitemonitor/SiteMonitorRecorder.java
+++ src/main/java/hudson/plugins/sitemonitor/SiteMonitorRecorder.java
@@ -113,7 +113,20 @@ public class SiteMonitorRecorder extends Recorder {
                 }
             });
             return connection;
-        } else {
+        }  else if(urlString.contains("@")){
+            URL passedURL = new URL(url);
+			String creds = url.substring(url.indexOf("//")+2, url.indexOf("@"));
+			String userName = creds.substring(0,creds.indexOf(":"));
+			String passWord = creds.substring(creds.indexOf(":")+1,creds.length());
+			HttpURLConnection connection = (HttpURLConnection) passedURL.openConnection();
+			String val = (new StringBuffer(userName).append(":").append(passWord)).toString();
+			byte[] base = val.getBytes();
+			String authorizationString = "Basic " + new String(new Base64().encode(base));
+			connection.setRequestProperty ("Authorization", authorizationString);
+			return connection;
+        }
+        
+        else {
             return (HttpURLConnection) ProxyConfiguration.open(new URL(urlString));
         }
     }
