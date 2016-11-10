fddfd0da1fc1ab4c285c3844f088525300cf2b14
Paul Wellner Bou <paul@wellnerbou.de>
2016-11-10 10:55:47 +0100

    Add readme and run script for local development

---
+++ README.md
@@ -0,0 +1,9 @@
+# SiteMonitor Jenkins Plugin
+
+## Development
+
+You can start a local jenkins with this plugin with:
+
+```
+    
+```
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf work/plugins
+MAVEN_OPTS="$MAVEN_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
+mvn -Dmaven.test.skip=true -DskipTests=true -Djetty.port=9191 clean compile org.jenkins-ci.tools:maven-hpi-plugin:run
