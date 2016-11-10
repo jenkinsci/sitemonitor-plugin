6df94eeada2bf2a62f9943f8ca2a8e7735e57715
Paul Wellner Bou <paul@wellnerbou.de>
2016-11-10 13:04:09 +0100

    Replace own implementation with Guava and avoid Nullpointer (happens in Jenkins 2)

---
+++ src/test/java/hudson/plugins/sitemonitor/mapper/SuccessCodeListToCvStringTest.java
@@ -0,0 +1,45 @@
+package hudson.plugins.sitemonitor.mapper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class SuccessCodeListToCvStringTest {
+
+    @Test
+    public void apply_withOneItem() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+
+        String result = SuccessCodeListToCvString.INSTANCE.apply(list);
+        Assert.assertEquals("1", result);
+    }
+
+    @Test
+    public void apply_withTwoItems() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        String result = SuccessCodeListToCvString.INSTANCE.apply(list);
+        Assert.assertEquals("1,2", result);
+    }
+
+    @Test
+    public void apply_withEmptyList() {
+        List<Integer> list = new ArrayList<>();
+        String result = SuccessCodeListToCvString.INSTANCE.apply(list);
+        Assert.assertEquals("", result);
+    }
+
+    @Test
+    public void apply_avoidNullPointer() {
+        String result = SuccessCodeListToCvString.INSTANCE.apply(null);
+        Assert.assertEquals("", result);
+    }
+
+}
